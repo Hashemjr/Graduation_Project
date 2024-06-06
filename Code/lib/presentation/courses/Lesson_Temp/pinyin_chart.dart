ca38db5895b24d87022ba9854e0b43e4ddf88c5f
boody75 <aljmal88@gmail.com>
2024-06-06 22:21:24 +0300

    lesson1 is done

--- Code/lib/presentation/courses/Lesson_Temp/pinyin_chart.dart
+++ Code/lib/presentation/courses/Lesson_Temp/pinyin_chart.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:audioplayers/audioplayers.dart';
 
 class PinyinChart extends StatefulWidget {
@@ -13,89 +11,69 @@ class _PinyinChartState extends State<PinyinChart> {
   late AudioPlayer _audioPlayer;
 
   final Map<String, String> pinyinMap = {
-'ā':'a1', 'á':'a2', 'ǎ':'a3', 'à':'a4',
-  'ō':'o1', 'ó':'o2', 'ǒ':'o3', 'ò':'o4',
-  'ē':'e1', 'é':'e2', 'ě':'e3', 'è':'e4',
-
-  'bā':'ba1', 'bá':'ba2', 'bǎ':'ba3', 'bà':'ba4',
-  'pā':'pa1', 'pá':'pa2', 'pǎ':'pa3', 'pà':'pa4',
-  'mā':'ma1', 'má':'ma2', 'mǎ':'ma3', 'mà':'ma4',
-  'fā':'fa1', 'fá':'fa2', 'fǎ':'fa3', 'fà':'fa4',
-
-  'dā':'da1', 'dá':'da2', 'dǎ':'da3', 'dà':'da4',
-  'tā':'ta1', 'tá':'ta2', 'tǎ':'ta3', 'tà':'ta4',
-  'nā':'na1', 'ná':'na2', 'nǎ':'na3', 'nà':'na4',
-  'lā':'la1', 'lá':'la2', 'lǎ':'la3', 'là':'la4',
-
-  'gā':'ga1', 'gá':'ga2', 'gǎ':'ga3', 'gà':'ga4',
-  'kā':'ka1', 'ká':'ka2', 'kǎ':'ka3', 'kà':'ka4',
-  'hā':'ha1', 'há':'ha2', 'hǎ':'ha3', 'hà':'ha4',
-
-  'zhā':'zha1', 'zhá':'zha2', 'zhǎ':'zha3', 'zhà':'zha4',
-  'chā':'cha1', 'chá':'cha2', 'chǎ':'cha3', 'chà':'cha4',
-  'shā':'sha1', 'shá':'sha2', 'shǎ':'sha3', 'shà':'sha4',
-
-  'zā':'za1', 'zá':'za2', 'zǎ':'za3', 'zà':'za4',
-  'cā':'ca1', 'cá':'ca2', 'cǎ':'ca3', 'cà':'ca4',
-  'sā':'sa1', 'sá':'sa2', 'sǎ':'sa3', 'sà':'sa4',
-
-  'yā':'ya1', 'yá':'ya2', 'yǎ':'ya3', 'yà':'ya4',
-  'wā':'wa1', 'wá':'wa2', 'wǎ':'wa3', 'wà':'wa4',
-
-  'yī':'yi1', 'yí':'yi2', 'yǐ':'yi3', 'yì':'yi4',
-  'wū':'wu1', 'wú':'wu2', 'wǔ':'wu3', 'wù':'wu4',
-  'yū':'yu1', 'yú':'yu2', 'yǔ':'yu3', 'yù':'yu4',
-
-  'bō':'bo1', 'bó':'bo2', 'bǒ':'bo3', 'bò':'bo4',
-  'pō':'po1', 'pó':'po2', 'pǒ':'po3', 'pò':'po4',
-  'mō':'mo1', 'mó':'mo2', 'mǒ':'mo3', 'mò':'mo4',
-  'fō':'fo1', 'fó':'fo2', 'fǒ':'fo3', 'fò':'fo4',
-
-  'dē':'de1', 'dé':'de2', 'dě':'de3', 'dè':'de4',
-  'tē':'te1', 'té':'te2', 'tě':'te3', 'tè':'te4',
-  'nē':'ne1', 'né':'ne2', 'ně':'ne3', 'nè':'ne4',
-  'lē':'le1', 'lé':'le2', 'lě':'le3', 'lè':'le4',
-
-  'gē':'ge1', 'gé':'ge2', 'gě':'ge3', 'gè':'ge4',
-  'kē':'ke1', 'ké':'ke2', 'kě':'ke3', 'kè':'ke4',
-  'hē':'he1', 'hé':'he2', 'hě':'he3', 'hè':'he4',
-
-  'jī':'ji1', 'jí':'ji2', 'jǐ':'ji3', 'jì':'ji4',
-  'qī':'qi1', 'qí':'qi2', 'qǐ':'qi3', 'qì':'qi4',
-  'xī':'xi1', 'xí':'xi2', 'xǐ':'xi3', 'xì':'xi4',
-
-  'zhī':'zhi1', 'zhí':'zhi2', 'zhǐ':'zhi3', 'zhì':'zhi4',
-  'chī':'chi1', 'chí':'chi2', 'chǐ':'chi3', 'chì':'chi4',
-  'shī':'shi1', 'shí':'shi2', 'shǐ':'shi3', 'shì':'shi4',
-  'rī':'ri1', 'rí':'ri2', 'rǐ':'ri3', 'rì':'ri4',
-
-  'zī':'zi1', 'zí':'zi2', 'zǐ':'zi3', 'zì':'zi4',
-  'cī':'ci1', 'cí':'ci2', 'cǐ':'ci3', 'cì':'ci4',
-  'sī':'si1', 'sí':'si2', 'sǐ':'si3', 'sì':'si4',
-
-  'yī':'yi1', 'yí':'yi2', 'yǐ':'yi3', 'yì':'yi4',
-  'wū':'wu1', 'wú':'wu2', 'wǔ':'wu3', 'wù':'wu4',
-  'yū':'yu1', 'yú':'yu2', 'yǔ':'yu3', 'yù':'yu4',
-
-  'juān':'juan1', 'juán':'juan2', 'juǎn':'juan3', 'juàn':'juan4',
-  'quān':'quan1', 'quán':'quan2', 'quǎn':'quan3', 'quàn':'quan4',
-  'xuān':'xuan1', 'xuán':'xuan2', 'xuǎn':'xuan3', 'xuàn':'xuan4',
-
-  'yān':'yan1', 'yán':'yan2', 'yǎn':'yan3', 'yàn':'yan4',
-  'wān':'wan1', 'wán':'wan2', 'wǎn':'wan3', 'wàn':'wan4',
-
-  'zhāng':'zhang1', 'zháng':'zhang2', 'zhǎng':'zhang3', 'zhàng':'zhang4',
-  'chāng':'chang1', 'cháng':'chang2', 'chǎng':'chang3', 'chàng':'chang4',
-  'shāng':'shang1', 'sháng':'shang2', 'shǎng':'shang3', 'shàng':'shang4',
-  'rāng':'rang1', 'ráng':'rang2', 'rǎng':'rang3', 'ràng':'rang4',
-
-  'zāng':'zang1', 'záng':'zang2', 'zǎng':'zang3', 'zàng':'zang4',
-  'cāng':'cang1', 'cáng':'cang2', 'cǎng':'cang3', 'càng':'cang4',
-  'sāng':'sang1', 'sáng':'sang2', 'sǎng':'sang3', 'sàng':'sang4',
-
-  'yīng':'ying1', 'yíng':'ying2', 'yǐng':'ying3', 'yìng':'ying4',
-  'wēng':'weng1', 'wéng':'weng2', 'wěng':'weng3', 'wèng':'weng4',
-
+    'ā':'a1', 'á':'a2', 'ǎ':'a3', 'à':'a4',
+    'ō':'o1', 'ó':'o2', 'ǒ':'o3', 'ò':'o4',
+    'ē':'e1', 'é':'e2', 'ě':'e3', 'è':'e4',
+    'bā':'ba1', 'bá':'ba2', 'bǎ':'ba3', 'bà':'ba4',
+    'pā':'pa1', 'pá':'pa2', 'pǎ':'pa3', 'pà':'pa4',
+    'mā':'ma1', 'má':'ma2', 'mǎ':'ma3', 'mà':'ma4',
+    'fā':'fa1', 'fá':'fa2', 'fǎ':'fa3', 'fà':'fa4',
+    'dā':'da1', 'dá':'da2', 'dǎ':'da3', 'dà':'da4',
+    'tā':'ta1', 'tá':'ta2', 'tǎ':'ta3', 'tà':'ta4',
+    'nā':'na1', 'ná':'na2', 'nǎ':'na3', 'nà':'na4',
+    'lā':'la1', 'lá':'la2', 'lǎ':'la3', 'là':'la4',
+    'gā':'ga1', 'gá':'ga2', 'gǎ':'ga3', 'gà':'ga4',
+    'kā':'ka1', 'ká':'ka2', 'kǎ':'ka3', 'kà':'ka4',
+    'hā':'ha1', 'há':'ha2', 'hǎ':'ha3', 'hà':'ha4',
+    'zhā':'zha1', 'zhá':'zha2', 'zhǎ':'zha3', 'zhà':'zha4',
+    'chā':'cha1', 'chá':'cha2', 'chǎ':'cha3', 'chà':'cha4',
+    'shā':'sha1', 'shá':'sha2', 'shǎ':'sha3', 'shà':'sha4',
+    'zā':'za1', 'zá':'za2', 'zǎ':'za3', 'zà':'za4',
+    'cā':'ca1', 'cá':'ca2', 'cǎ':'ca3', 'cà':'ca4',
+    'sā':'sa1', 'sá':'sa2', 'sǎ':'sa3', 'sà':'sa4',
+    'yā':'ya1', 'yá':'ya2', 'yǎ':'ya3', 'yà':'ya4',
+    'wā':'wa1', 'wá':'wa2', 'wǎ':'wa3', 'wà':'wa4',
+    'yī':'yi1', 'yí':'yi2', 'yǐ':'yi3', 'yì':'yi4',
+    'wū':'wu1', 'wú':'wu2', 'wǔ':'wu3', 'wù':'wu4',
+    'yū':'yu1', 'yú':'yu2', 'yǔ':'yu3', 'yù':'yu4',
+    'bō':'bo1', 'bó':'bo2', 'bǒ':'bo3', 'bò':'bo4',
+    'pō':'po1', 'pó':'po2', 'pǒ':'po3', 'pò':'po4',
+    'mō':'mo1', 'mó':'mo2', 'mǒ':'mo3', 'mò':'mo4',
+    'fō':'fo1', 'fó':'fo2', 'fǒ':'fo3', 'fò':'fo4',
+    'dē':'de1', 'dé':'de2', 'dě':'de3', 'dè':'de4',
+    'tē':'te1', 'té':'te2', 'tě':'te3', 'tè':'te4',
+    'nē':'ne1', 'né':'ne2', 'ně':'ne3', 'nè':'ne4',
+    'lē':'le1', 'lé':'le2', 'lě':'le3', 'lè':'le4',
+    'gē':'ge1', 'gé':'ge2', 'gě':'ge3', 'gè':'ge4',
+    'kē':'ke1', 'ké':'ke2', 'kě':'ke3', 'kè':'ke4',
+    'hē':'he1', 'hé':'he2', 'hě':'he3', 'hè':'he4',
+    'jī':'ji1', 'jí':'ji2', 'jǐ':'ji3', 'jì':'ji4',
+    'qī':'qi1', 'qí':'qi2', 'qǐ':'qi3', 'qì':'qi4',
+    'xī':'xi1', 'xí':'xi2', 'xǐ':'xi3', 'xì':'xi4',
+    'zhī':'zhi1', 'zhí':'zhi2', 'zhǐ':'zhi3', 'zhì':'zhi4',
+    'chī':'chi1', 'chí':'chi2', 'chǐ':'chi3', 'chì':'chi4',
+    'shī':'shi1', 'shí':'shi2', 'shǐ':'shi3', 'shì':'shi4',
+    'rī':'ri1', 'rí':'ri2', 'rǐ':'ri3', 'rì':'ri4',
+    'zī':'zi1', 'zí':'zi2', 'zǐ':'zi3', 'zì':'zi4',
+    'cī':'ci1', 'cí':'ci2', 'cǐ':'ci3', 'cì':'ci4',
+    'sī':'si1', 'sí':'si2', 'sǐ':'si3', 'sì':'si4',
+    'yī':'yi1', 'yí':'yi2', 'yǐ':'yi3', 'yì':'yi4',
+    'wū':'wu1', 'wú':'wu2', 'wǔ':'wu3', 'wù':'wu4',
+    'yū':'yu1', 'yú':'yu2', 'yǔ':'yu3', 'yù':'yu4',
+    'juān':'juan1', 'juán':'juan2', 'juǎn':'juan3', 'juàn':'juan4',
+    'quān':'quan1', 'quán':'quan2', 'quǎn':'quan3', 'quàn':'quan4',
+    'xuān':'xuan1', 'xuán':'xuan2', 'xuǎn':'xuan3', 'xuàn':'xuan4',
+    'yān':'yan1', 'yán':'yan2', 'yǎn':'yan3', 'yàn':'yan4',
+    'wān':'wan1', 'wán':'wan2', 'wǎn':'wan3', 'wàn':'wan4',
+    'zhāng':'zhang1', 'zháng':'zhang2', 'zhǎng':'zhang3', 'zhàng':'zhang4',
+    'chāng':'chang1', 'cháng':'chang2', 'chǎng':'chang3', 'chàng':'chang4',
+    'shāng':'shang1', 'sháng':'shang2', 'shǎng':'shang3', 'shàng':'shang4',
+    'rāng':'rang1', 'ráng':'rang2', 'rǎng':'rang3', 'ràng':'rang4',
+    'zāng':'zang1', 'záng':'zang2', 'zǎng':'zang3', 'zàng':'zang4',
+    'cāng':'cang1', 'cáng':'cang2', 'cǎng':'cang3', 'càng':'cang4',
+    'sāng':'sang1', 'sáng':'sang2', 'sǎng':'sang3', 'sàng':'sang4',
+    'yīng':'ying1', 'yíng':'ying2', 'yǐng':'ying3', 'yìng':'ying4',
+    'wēng':'weng1', 'wéng':'weng2', 'wěng':'weng3', 'wèng':'weng4',
   };
 
   @override
@@ -129,25 +107,48 @@ class _PinyinChartState extends State<PinyinChart> {
 
   @override
   Widget build(BuildContext context) {
+    final double cardPadding = MediaQuery.of(context).size.width * 0.02;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Interactive Pinyin Chart',style: TextStyle(
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                  fontSize: 25.0,
-                  ),),
+        title: Text(
+          'Interactive Pinyin Chart',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blueAccent,
+        elevation: 0,
       ),
       body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+        padding: EdgeInsets.all(cardPadding),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: cardPadding,
+          mainAxisSpacing: cardPadding,
+        ),
         itemCount: pinyinMap.keys.length,
         itemBuilder: (context, index) {
           String pinyin = pinyinMap.keys.elementAt(index);
           return GestureDetector(
-            onTap: () => _playSound(pinyinMap[pinyin]!),//
+            onTap: () => _playSound(pinyinMap[pinyin]!),
             child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 5,
               child: Center(
                 child: Text(
                   pinyin,
-                  style: TextStyle(fontSize: 24,color: const Color.fromARGB(255, 0, 0, 0),),
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
               ),
             ),
@@ -156,4 +157,11 @@ class _PinyinChartState extends State<PinyinChart> {
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: PinyinChart(),
+  ));
 }
--- Code/lib/presentation/courses/Lesson_Temp/tone_practice.dart
+++ Code/lib/presentation/courses/Lesson_Temp/tone_practice.dart
@@ -1,35 +1,205 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-class TonePractice extends StatelessWidget {
-  final List<String> tones = ['ā', 'á', 'ǎ', 'à'];
-  final List<String> syllables = ['ma', 'ba', 'da', 'fa'];
+class TonePractice extends StatefulWidget {
+  @override
+  _TonePracticeState createState() => _TonePracticeState();
+}
+
+class _TonePracticeState extends State<TonePractice> {
+  final List<String> tones = ['ā', 'á', 'ǎ', 'à', 'ē', 'é', 'ě', 'è'];
+  final List<String> syllables = ['ma', 'ba', 'da', 'fa', 'mo', 'bo', 'do', 'fo'];
+  late AudioPlayer _audioPlayer;
+  final Map<String, String> selectedTones = {};
+  final Map<String, bool> answerStatus = {}; // Track correctness of answers
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+void _playSound(String syllable, String tone) async {
+  if (!mounted) return; // Check if the widget is still mounted
+  
+  String pinyin = syllable + tone.substring(1); // Concatenate syllable with tone number
+  String url = 'https://yabla.vo.llnwd.net/media.yabla.com/chinese_static/audio/alicia/$pinyin.mp3';
+  try {
+    await _audioPlayer.setSourceUrl(url); // Updated method name
+    await _audioPlayer.resume();
+  } catch (e) {
+    if (mounted) { // Check again if widget is still mounted before showing the SnackBar
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to load audio for $pinyin', style: TextStyle(color: Colors.red)),
+        ),
+      );
+    }
+  }
+}
+
+  void _checkAnswers() {
+    answerStatus.clear(); // Clear previous feedback
+    for (int i = 0; i < syllables.length; i++) {
+      String syllable = syllables[i];
+      if (selectedTones.containsKey(syllable)) {
+        String selectedTone = selectedTones[syllable]!;
+        String correctTone = tones[i]; // Correct tone is based on the syllable index
+        answerStatus[syllable] = selectedTone == correctTone; // Mark correctness
+      }
+    }
+    setState(() {}); // Update UI to reflect feedback
+  }
+
+  void _resetAnswers() {
+    selectedTones.clear();
+    answerStatus.clear();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
+    final double cardPadding = MediaQuery.of(context).size.width * 0.02;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tone Practice'),
+        title: Text(
+          'Tone Practice',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blueAccent,
+        elevation: 0,
       ),
       body: ListView.builder(
+        padding: EdgeInsets.all(cardPadding),
         itemCount: syllables.length,
         itemBuilder: (context, index) {
           String syllable = syllables[index];
-          return ListTile(
-            title: Text(syllable),
-            trailing: DropdownButton<String>(
-              items: tones.map((String tone) {
-                return DropdownMenuItem<String>(
-                  value: tone,
-                  child: Text(tone,style: TextStyle(fontSize: 24,color: const Color.fromARGB(255, 0, 0, 0),)),
-                );
-              }).toList(),
-              onChanged: (String? value) {
-                // Handle tone selection
-              },
+          bool isCorrect = answerStatus.containsKey(syllable) ? answerStatus[syllable]! : false;
+          Color textColor = isCorrect ? Colors.green : Colors.black;
+
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 5,
+            margin: EdgeInsets.symmetric(vertical: cardPadding),
+            child: ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: cardPadding, vertical: cardPadding / 2),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    syllable,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: textColor, // Change color based on correctness
+                    ),
+                  ),
+                  if (selectedTones[syllable] != null)
+                    Text(
+                      selectedTones[syllable]!,
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 24,
+                        color: textColor,
+                      ),
+                    ),
+                ],
+              ),
+              trailing: DropdownButton<String>(
+                underline: Container(),
+                items: tones.map((String tone) {
+                  return DropdownMenuItem<String>(
+                    value: tone,
+                    child: Text(
+                      tone,
+                      style: TextStyle(fontSize: 24, color: textColor),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  if (value != null) {
+                    setState(() {
+                      selectedTones[syllable] = value;
+                    });
+                    _playSound(syllable, value);
+                  }
+                },
+                icon: Icon(Icons.arrow_drop_down, color: textColor),
+              ),
             ),
           );
         },
       ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(cardPadding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: _checkAnswers,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blueAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30), // Increased border radius
+                ),
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                elevation: 5, // Added elevation
+              ),
+              child: Text(
+                'Check Answers',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _resetAnswers,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.redAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30), // Increased border radius
+                ),
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                elevation: 5, // Added elevation
+              ),
+              child: Text(
+                'Reset',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TonePractice(),
+  ));
 }
